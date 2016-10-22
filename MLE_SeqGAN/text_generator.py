c42aa1ae995b1c61f2e80dc07d9d61ee760a3d1e
Niko Gamulin <niko@cloudmondo.com>
2016-10-22 20:57:50 +0200

    modified text_generator

--- MLE_SeqGAN/text_generator.py
+++ MLE_SeqGAN/text_generator.py
@@ -1,11 +1,24 @@
+# -*- coding: utf-8 -*-
+
 import string
 import re
 import nltk
 import cPickle as pickle
 from random import randint
 
+caps = "([A-Z])"
+prefixes = "(Mr|St|Mrs|Ms|Dr)[.]"
+suffixes = "(Inc|Ltd|Jr|Sr|Co)"
+starters = "(Mr|Mrs|Ms|Dr|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
+acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
+websites = "[.](com|net|org|io|gov)"
+
+wordsToIgnore = ["\n", "''", '``']
+
+
+
 class TextGenerator:
-    def __init__(self, index2WordFile, word2IndexFile, corpus):
+    def __init__(self, index2WordFile, word2IndexFile, corpus, sentenceLengthLimit = 20):
         self.index2WordFile = index2WordFile
         self.word2IndexFile = word2IndexFile
         self.sequenceOfIndices = []
@@ -14,9 +27,16 @@ class TextGenerator:
         self.corpusWordsCount = None
 
         with open(corpus, 'r') as f:
-            content = ''.join(f.readlines()).replace("\n", "")
+            content = ' '.join(f.readlines())
+            for wordToIgnore in wordsToIgnore:
+                content = content.replace(wordToIgnore, '')
+            self.sentences = [nltk.word_tokenize(sentence) for sentence in self.splitIntoSentences(content) if len(sentence) <= sentenceLengthLimit]
+            self.sentencesCount = len(self.sentences)
             words = nltk.word_tokenize(content)
             self.tokens = [word.lower() for word in words]
+            #add blank space
+            self.word2Index[''] = [0]
+            self.index2Word[0] = ''
             for word in self.tokens:
                 if word in self.word2Index:
                     index = self.word2Index[word]
@@ -34,10 +54,46 @@ class TextGenerator:
 
         self.corpusWordsCount = len(self.tokens)
 
+    def splitIntoSentences(self, text):
+        text = " " + text + "  "
+        text = text.replace("\n", " ")
+        text = re.sub(prefixes, "\\1<prd>", text)
+        text = re.sub(websites, "<prd>\\1", text)
+        if "Ph.D" in text: text = text.replace("Ph.D.", "Ph<prd>D<prd>")
+        text = re.sub("\s" + caps + "[.] ", " \\1<prd> ", text)
+        text = re.sub(acronyms + " " + starters, "\\1<stop> \\2", text)
+        text = re.sub(caps + "[.]" + caps + "[.]" + caps + "[.]", "\\1<prd>\\2<prd>\\3<prd>", text)
+        text = re.sub(caps + "[.]" + caps + "[.]", "\\1<prd>\\2<prd>", text)
+        text = re.sub(" " + suffixes + "[.] " + starters, " \\1<stop> \\2", text)
+        text = re.sub(" " + suffixes + "[.]", " \\1<prd>", text)
+        text = re.sub(" " + caps + "[.]", " \\1<prd>", text)
+        if "”" in text: text = text.replace(".”", "”.")
+        if "\"" in text: text = text.replace(".\"", "\".")
+        if "!" in text: text = text.replace("!\"", "\"!")
+        if "?" in text: text = text.replace("?\"", "\"?")
+        text = text.replace(".", ".<stop>")
+        text = text.replace("?", "?<stop>")
+        text = text.replace("!", "!<stop>")
+        text = text.replace("<prd>", ".")
+        sentences = text.split("<stop>")
+        sentences = sentences[:-1]
+        sentences = [s.strip() for s in sentences]
+        return sentences
+
     def generateSequence(self, length):
-        maxIndex = self.corpusWordsCount - length
-        startIndex = randint(0, maxIndex)
-        return self.sequenceOfIndices[startIndex: startIndex + length]
+        if self.sentencesCount == 0:
+            maxIndex = self.corpusWordsCount - length
+            startIndex = randint(0, maxIndex)
+            return self.sequenceOfIndices[startIndex: startIndex + length]
+        else:
+            sentenceIndex = randint(0, self.sentencesCount - 1)
+            wordsSequence = self.sentences[sentenceIndex]
+            tokensSequence = [self.word2Index[word.lower()] for word in wordsSequence]
+            spacesToAppend = length - len(tokensSequence)
+            if spacesToAppend > 0:
+                tokensSequence += [0] * spacesToAppend
+            return tokensSequence
+
 
     def saveSamplesToFile(self, length, samplesCount, fileToSave):
         samples = [self.generateSequence(length) for _ in range(samplesCount)]
@@ -75,6 +131,13 @@ class TextGenerator:
 if __name__ == "__main__":
     testInput = "./target_generate/generator_sample.txt"
     generator = TextGenerator('index2word.pickle', 'word2index.pickle', '../corpus_tools/data/source/dickens.txt')
+
+    for _ in range(20):
+        testSequenceIndices = generator.generateSequence(20)
+        testSequenceWords = [generator.index2Word[index] for index in testSequenceIndices if index in generator.index2Word]
+        print " ".join(testSequenceWords)
+
+
     with open(testInput, 'r') as f:
         sentences = f.readlines()
 
